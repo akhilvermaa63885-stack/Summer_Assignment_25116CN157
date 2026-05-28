e015674b1df6ea355fe2120151d6c73ea99f6a05
AKHIL_003 <akhilvermaa63885@gmail.com>
2026-05-28 13:46:22 +0530

    Update day 1.c

--- day 1.c
+++ day 1.c
@@ -33,7 +33,7 @@ int main() {
     printf("Multiplication Table of %d:\n", num);
 
     for(i = 1; i <= 10; i++) {
-        printf("%d x %d = %d\n", num, i, num * i);
+        printf("%d * %d = %d\n", num, i, num * i);
     }
 
     return 0;
