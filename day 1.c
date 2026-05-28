b672c4fc03d47cdf714d818609e4f29bbf5c4bfd
AKHIL_003 <akhilvermaa63885@gmail.com>
2026-05-28 13:25:06 +0530

    Update day 1.c

--- day 1.c
+++ day 1.c
@@ -3,3 +3,20 @@ int main(void) {
     printf("Hello, world!\n");
     return 0;
 }
+
+***Write a program to Calculate sum of first N 
+natural numbers.***
+
+#include <stdio.h>
+
+int main() {
+     int n, sum = 0;
+
+    printf("Enter a positive integer N: ");
+    scanf("%d", &n);
+    sum = n * (n + 1) / 2;
+
+    printf("sum of first natural numbers is: %d\n", sum);
+
+    return 0;
+}
